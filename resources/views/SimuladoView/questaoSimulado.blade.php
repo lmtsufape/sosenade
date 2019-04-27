2208aca1b9d8e943cbc7c29d187c7f6337e290c0
djeymisson <djeymisson@gmail.com>
2019-04-26 23:33:16 -0300

    Adicionado PopOver de erro.

--- resources/views/SimuladoView/questaoSimulado.blade.php
+++ resources/views/SimuladoView/questaoSimulado.blade.php
@@ -16,7 +16,7 @@
 				<div class="card-header">
 					<h4 class="card-text"> {!! $questao['enunciado']!!} </h4>
 				</div>
-				<div class="card-body col-md-12 form-group">
+				<div class="card-body">
 					<h5 class="card-title">Selecione a resposta correta:</h5>
 					<div class="list-group container">
 						<input type="radio" name="alternativa" id="radioa" value="0" required autofocus />
@@ -34,8 +34,8 @@
 						<input type="radio" name="alternativa" id="radioe" value="4" />
 						<label class="list-group-item" for="radioe">{{$questao['alternativa_e']}}</label>
 					</div>
-					<div class="col-md-12 mt-3 text-center">
-						<button type="submit" class="btn btn-success pull-center" data-container="body" data-toggle="popover" data-placement="right" data-content="Selecione uma alternativa para prosseguir.">Próxima</button>
+					<div class="col-md-12 mt-4 text-center">
+						<button onclick="atLeastOneRadio()" id="confirmar-btn" type="submit" class="btn btn-success pull-center" data-container="body" data-toggle="popover" data-placement="right" data-content="Selecione uma alternativa para prosseguir.">Próxima</button>
 					</div>
 				</div>
 			</div>
@@ -70,10 +70,26 @@
 </style>
 
 <script type="text/javascript">
+	function atLeastOneRadio() {
+		var chx = document.getElementsByTagName('input');
+		for (var i=0; i<chx.length; i++) {
+			// If you have more than one radio group, also check the name attribute
+			// for the one you want as in && chx[i].name == 'choose'
+			// Return true from the function on first match of a checked item
+			if (chx[i].type == 'radio' && chx[i].checked) {
+				return true;
+			} 
+		}
+		// End of the loop, return false
+		$('#confirmar-btn').popover('show');
+		setTimeout(function() {
+		    $('#confirmar-btn').popover('hide');
+		}, 1500);
+	}
+
 	$(function () {
-		$('[data-toggle="popover"]').popover()
-		$('.example-popover').popover({
-			container: 'body'
+		$('#confirmar-btn').popover({
+			trigger: 'manual'
 		})
 	})
 </script>
