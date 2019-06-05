2a46d64a7361ec5982805ac29e9f85480c2e6197
djeymisson <djeymisson@gmail.com>
2019-06-05 16:39:44 -0300

    Corrigido comportamento inesperado de Modals na montagem de simulados.

--- resources/views/SimuladoView/montarSimulado.blade.php
+++ resources/views/SimuladoView/montarSimulado.blade.php
@@ -96,43 +96,42 @@
 											<a class="icons btn btn-sm btn-info" data-toggle="modal" href="#modal_{{$qst->questao->id}}" data-placement="bottom" rel="tooltip" title="Visualizar"><i class="fa fa-eye"></i></a>
 											<a href="{{route('edit_qst', ['id'=>$qst->questao->id])}}" class="btn btn-sm btn-primary" data-placement="bottom" rel="tooltip" title="Editar"><i class="fa fa-pencil"></i></a>
 											<a onclick="return confirm('Você tem certeza que deseja remover?')" href="{{route('remove_qst_simulado', ['sim_qst_id'=>$qst->id])}}" class="btn btn-sm btn-danger" data-placement="bottom" rel="tooltip" title="Excluir"><i class="fa fa-trash"></i></a>
-										</td>
-									</tr>
-
-									<!-- Modal -->
-									<div class="modal fade" id="modal_{{$qst->questao->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-										<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-											<div class="modal-content">
-												<div class="modal-header">
-													<h5 class="modal-title" id="modalTitle_{{$qst->questao->id}}">{{$qst->questao->disciplina->nome}} - {{$qst->questao->dificuldade}}</h5>
-													<button type="button" class="close" data-dismiss="modal" aria-label="Voltar">
-														<span aria-hidden="true">&times;</span>
-													</button>
-												</div>
-												<div class="modal-body" style="overflow: hidden; word-wrap: break-word;">
-													<div class="row">
-														<div class="card-header w-100">
-															<span> {!! $qst->questao->toArray()['enunciado'] !!} </span>
+											<!-- Modal -->
+											<div class="modal fade" id="modal_{{$qst->questao->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+												<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+													<div class="modal-content">
+														<div class="modal-header">
+															<h5 class="modal-title" id="modalTitle_{{$qst->questao->id}}">{{$qst->questao->disciplina->nome}} - {{$qst->questao->dificuldade}}</h5>
+															<button type="button" class="close" data-dismiss="modal" aria-label="Voltar">
+																<span aria-hidden="true">&times;</span>
+															</button>
 														</div>
-														<div class="card-body">
-															<h5 class="card-title">Alternativas:</h5>
-															<div class="list-group container">
-																<span class="list-group-item {{  $qst->questao->alternativa_correta == '0' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_a'] !!}</span>
-																<span class="list-group-item {{  $qst->questao->alternativa_correta == '1' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_b'] !!}</span>
-																<span class="list-group-item {{  $qst->questao->alternativa_correta == '2' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_c'] !!}</span>
-																<span class="list-group-item {{  $qst->questao->alternativa_correta == '3' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_d'] !!}</span>
-																<span class="list-group-item {{  $qst->questao->alternativa_correta == '4' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_e'] !!}</span>
+														<div class="modal-body" style="overflow: hidden; word-wrap: break-word;">
+															<div class="row">
+																<div class="card-header w-100">
+																	<span> {!! $qst->questao->toArray()['enunciado'] !!} </span>
+																</div>
+																<div class="card-body">
+																	<h5 class="card-title">Alternativas:</h5>
+																	<div class="list-group container">
+																		<span class="list-group-item {{  $qst->questao->alternativa_correta == '0' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_a'] !!}</span>
+																		<span class="list-group-item {{  $qst->questao->alternativa_correta == '1' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_b'] !!}</span>
+																		<span class="list-group-item {{  $qst->questao->alternativa_correta == '2' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_c'] !!}</span>
+																		<span class="list-group-item {{  $qst->questao->alternativa_correta == '3' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_d'] !!}</span>
+																		<span class="list-group-item {{  $qst->questao->alternativa_correta == '4' ? 'list-group-item-success' : '' }}">{!! $qst->questao->toArray()['alternativa_e'] !!}</span>
+																	</div>
+																</div>
 															</div>
+														</div>
+														<div class="modal-footer">
+															<a href="{{route('edit_qst', ['id'=>$qst->questao->id])}}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+															<a onclick="return confirm('Você tem certeza que deseja remover?')" href="{{route('remove_qst_simulado', 	['sim_qst_id'=>$qst->id])}}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
 														</div>
 													</div>
 												</div>
-												<div class="modal-footer">
-													<a href="{{route('edit_qst', ['id'=>$qst->questao->id])}}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
-													<a onclick="return confirm('Você tem certeza que deseja remover?')" href="{{route('remove_qst_simulado', 	['sim_qst_id'=>$qst->id])}}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-												</div>
 											</div>
-										</div>
-									</div>
+										</td>
+									</tr>
 								@endforeach
 						</tbody>
 					</table>
