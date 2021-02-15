8bff1afe7613245ec3a2783dafe788c95e413029
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-02-15 17:12:18 -0300

    Redesign do footer concluído, novos icones adicionado

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,78 @@
-<footer class="position-absolute" id="footer" style="background-color: #1B2E4F; bottom: 0; width: 100%; height: 60px">
-	<div class="container py-2">
-		<!-- Footer Brasil -->
-		<div id="footer-brasil"></div>
-	</div>
-</footer>
+<div class="" style="background-color: white; width: 100%; color:black; margin-top:1.5rem;
+padding-bottom: 40px; position:absolute; bottom:100; width:100%; ">
+    <div class="container-fluid pb-0 mb-0 justify-content-center text-black ">
+        <div class="row justify-content-center"
+             style="padding: 10px;">
+            <div class="col-sm-3" align="center">
+                <div class="row justify-content-center" style="margin-top:15px;">
+                    <div class="col-sm-12" style="font-family:arial; ">
+                        <a href="" style="text-decoration: none; color:black">Início</a>  ●
+                        <a href="" style="text-decoration: none; color:black">Sobre</a>
+                        <hr style=" border: 0; height: 1px; background: #333; background-image: linear-gradient(to right, #ccc, #333, #ccc);">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center" style="">
+            <div class="col-sm-4" align="center">
+                <div class="row justify-content-center" style="margin-top:10px; margin-top:1.4rem;">
+                    <div class="col-sm-12" id="" style="font-weight:bold; font-family:arial;">
+                        Desenvolvido por
+                    </div>
+                    <div style="margin:3px; margin-top:1.4rem;">
+                        <a href="http://lmts.uag.ufrpe.br/" target="blank">
+                            <img src="{{ asset('\images\logo_lmts_colorido.png') }}" style="height: 80px">
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-4" align="center">
+                <div class="row justify-content-center" style="margin-top:10px; margin-top:1.4rem;">
+                    <div class="col-sm-12" id="" style="font-weight:bold; font-family:arial">
+                        Apoio
+                    </div>
+                    <div style="margin: 3px;">
+                        <a href="http://lmts.uag.ufrpe.br/" target="blank">
+                            <img style="height: 100px" src="{{ asset('\images\logo_ufape_preto.png') }}">
+                        </a>
+                    </div>
+                    <div style="margin: 3px; margin-top:1.8rem;">
+                        <p style="display: inline">Universidade Federal do<br> Agreste de Pernambuco</p>
+                    </div>
+                    <div style="margin:3px; ">
+                        <a href="http://lmts.uag.ufrpe.br/" target="blank">
+                            <img style="height: 100px" src="{{ asset('\images\logo_ufrpe.png') }}">
+                        </a>
+                    </div>
+                    <div style="margin:3px; margin-top:1.8rem;">
+                        <p style="display: inline">Universidade Federal<br> Rural de Pernambuco</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4" align="center">
+                <div class="row justify-content-center" style=" margin-top:1.4rem;">
+                    <div class="col-sm-12" id="" style="font-weight:bold; font-family:arial;">
+                        Redes sociais
+                    </div>
+                    <div style="margin:3px;  margin-top:2.4rem;">
+                        <a href="" target="blank">
+                            <img style="height: 30px" src="{{ asset('\images\logo_facebook_preto.png') }}">
+                        </a>
+                    </div>
+                    <div style="margin:3px; margin-top:2.4rem;">
+                        <a href="" target="blank">
+                            <img style="height: 30px" src="{{ asset('\images\logo_instagram_preto.png') }}">
+                        </a>
+                    </div>
+                    <div style="margin:3px; margin-top:2.4rem;">
+                        <a href="" target="blank">
+                            <img style="height: 30px" src="{{ asset('\images\logo_twitter_preto.png') }}">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="{{ asset('js/simulado_script.js') }}"></script>
