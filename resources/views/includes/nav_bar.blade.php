b38483fb229276d4c246fc7bfc3bc51d21b49148
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-02-17 15:55:37 -0300

    Sombra adicionado ao navbar

--- resources/views/includes/nav_bar.blade.php
+++ resources/views/includes/nav_bar.blade.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-light navbar-expand-lg" style="background-color: white; border-color: #d3e0e9" role="navigation">
+<nav class="navbar navbar-light navbar-expand-lg" style="background-color: white; border-color: #d3e0e9;
+box-shadow: 0px 4px 10px -5px rgba(0,0,0,0.64);" role="navigation">
 	<div class="container">
 		<a href="{{ (Auth::guard('aluno')->user()) ? route('home_aluno') : route('home') }}" style="max-height: 45%; max-width: 45%">
 			<img src="{{asset('1.png')}}" style="width: 20%" class="img-fluid float-left">
