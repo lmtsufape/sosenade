d9da903a3553108f6be164118ff772858939c6c7
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-02-24 10:58:45 -0300

    add arrays de validacoes

--- app/UnidadeAcademica.php
+++ app/UnidadeAcademica.php
@@ -5,7 +5,17 @@ namespace SimuladoENADE;
 use Illuminate\Database\Eloquent\Model;
 
 class UnidadeAcademica extends Model
-{
+{   
+    
+    public static $rules = 
+    [
+
+    ];
+
+    public static $messages = [
+
+    ];
+
     public function cursos(){
         return $this->hasMany('SimuladoENADE\Curso', 'unidade_id');
     }
