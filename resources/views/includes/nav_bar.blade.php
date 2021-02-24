4a34c893ab448002c3ebc061bcfef32cd3742583
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-02-24 10:46:18 -0300

    add routes de unidades

--- resources/views/includes/nav_bar.blade.php
+++ resources/views/includes/nav_bar.blade.php
@@ -61,8 +61,8 @@ box-shadow: 0px 4px 10px -5px rgba(0,0,0,0.64);" role="navigation">
 							Unidades
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="{{-- --}}">Cadastrar</a>
-							<a class="dropdown-item" href="{{-- --}}">Listar</a>
+							<a class="dropdown-item" href="{{ route('new_unidade') }}">Cadastrar</a>
+							<a class="dropdown-item" href="{{ route('list_unidade') }}">Listar</a>
 						</div>
 					</li>
 
