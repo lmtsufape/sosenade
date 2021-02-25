50aac08ccc1bc694e1ee2e38df693f8a1f9d7790
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-02-25 19:11:25 -0300

    add funcionalidade dos metodos

--- app/Http/Controllers/UnidadeAcademicaController.php
+++ app/Http/Controllers/UnidadeAcademicaController.php
@@ -5,30 +5,67 @@ namespace SimuladoENADE\Http\Controllers;
 use Illuminate\Http\Request;
 use SimuladoENADE\Validator\UnidadeAcademicaValidator;
 use SimuladoENADE\Validator\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 class UnidadeAcademicaController extends Controller
 {
     public function listar() {
-        dd(['list'=>'Listar Unidades']);
+        
+        $auth = \Auth::guard('instituicao')->user();
+
+        $unidades = \SimuladoENADE\UnidadeAcademica::where('instituicao_id', $auth->id)->get();
+
+        dd($unidades);
+        // return view('/path/nomeView', ['unidades' => $unidades]);
     }
 
     public function cadastrar() {
-        dd(['new'=>'Cadastrar Unidade']);
+        dd('View Cadastro');
+        // return view('/path/nomeView');
     }
 
     public function adicionar(Request $request) {
+        try{
+            $auth = \Auth::guard('instituicao')->user();
 
+            UnidadeAcademicaValidator::Validate($request->all());
+
+            $unidade = new \SimuladoENADE\UnidadeAcademica();
+            $unidade->fill($request->all());
+            $unidade->instituicao_id = $auth_id;
+            $unidade->save();
+
+            return redirect('/cadastrar/unidade');
+        }catch(ValidatorException $ex){
+            redirect('/cadastrar/unidade')->withErrors($ex->getValidator())->withInput();
+        }
     }
 
     public function editar(Request $request) {
-
+        $unidade = \SimuladoENADE\UnidadeAcademica::find($request->id);
+        // return view('/path/nomeView', ['unidade' => $unidade]);
     }
 
     public function atualizar(Request $request) {
+        try{
+            $auth = \Auth::guard('instituicao')->user();
 
+            UnidadeAcademicaValidator::Validate($request->all());
+
+            $unidade = \SimuladoENADE\UnidadeAcademica::find($request->id);
+            $unidade->fill($request->all());
+            $unidade->instituicao_id = $auth_id;
+            $unidade->update();
+
+            return redirect('/listar/unidade');
+        }catch(ValidatorException $ex){
+            redirect('/editar/unidade')->withErrors($ex->getValidator())->withInput();
+        }
     }
 
     public function remover(Request $request) {
-
+        $unidade = \SimuladoENADE\UnidadeAcademica::find($request->id);
+        $unidade->delete();
+        return redirect('/listar/unidade');
     }
 }
