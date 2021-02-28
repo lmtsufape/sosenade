7670610c299b2e52a9f886eb91f2924ac83e48ff
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-02-28 18:38:33 -0300

    add atributos para comunicação com banco de dados e add funções para formatação de dados vindos de consultas do banco de dados

--- app/UnidadeAcademica.php
+++ app/UnidadeAcademica.php
@@ -6,15 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class UnidadeAcademica extends Model
 {   
-    
-    public static $rules = 
-    [
+    protected $table = 'unidade_academicas';
+
+    protected $fillable = ['nome', 'instituicao_id'];
+
+    public static $rules = [
 
     ];
 
     public static $messages = [
 
     ];
+
+    public static function queryToStringIds($values) {
+        
+        $str = '(';
+        
+        foreach($values as $value)
+            $str = $str.$value->id.',';
+        
+        $virgula_final = (strlen($str)-1);
+        $str[$virgula_final] = ')';
+
+        return $str;
+    }
+
+    public static function queryToArrayIds($values) {
+        
+        $array = [];
+
+        foreach($values as $value)
+            array_push($array, $value->id);
+        
+        return $array;
+    }
 
     public function cursos(){
         return $this->hasMany('SimuladoENADE\Curso', 'unidade_id');
