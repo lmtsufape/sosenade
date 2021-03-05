4beae93a58b725e8b3ee679ce92c35d478e3d632
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-03-05 19:05:12 -0300

    retornando arquivo para a versão anterior

--- app/Http/Controllers/CicloController.php
+++ app/Http/Controllers/CicloController.php
@@ -5,21 +5,17 @@ namespace SimuladoENADE\Http\Controllers;
 use Illuminate\Http\Request;
 use SimuladoENADE\Validator\CicloValidator;
 use SimuladoENADE\Validator\ValidationException;
-use Illuminate\Support\Facades\DB;
+
 
 class Ciclocontroller extends Controller
 {
-
+    //
 	public function adicionar(Request $request){
     	try{
-
-			$auth = \Auth::guard('instituicao')->user();
-
         	CicloValidator::Validate($request->all());
 
         	$ciclo = new \SimuladoENADE\Ciclo();
         	$ciclo->fill($request->all());
-			$ciclo->instituicao_id = $auth->id;
         	$ciclo->save();
         	return redirect("listar/ciclo");
     	}
@@ -31,30 +27,20 @@ class Ciclocontroller extends Controller
 	public function cadastrar(){
     	return view('/CicloView/cadastrarCiclo');
 	}
-
 	public function listar(){
-
-		$auth = \Auth::guard('instituicao')->user();
-		$ciclos = \SimuladoENADE\Ciclo::where('instituicao_id', $auth->id)->get();
-		
+		$ciclos = \SimuladoENADE\Ciclo::all();
 		return view('/CicloView/listaCiclo', ['ciclos' => $ciclos]);
 	}
-
 	public function editar(Request $request){
 		$ciclos = \SimuladoENADE\Ciclo::find($request->id);
 		return view('/CicloView/editarCiclo',['ciclo' =>$ciclos]);
 	}
-
 	public function atualizar(Request $request){
 		try{
-
-			$auth = \Auth::guard('instituicao')->user();
-
         	CicloValidator::Validate($request->all());
 
 			$ciclo = \SimuladoENADE\Ciclo::find($request->id);
         	$ciclo->fill($request->all());
-			$ciclo->instituicao_id = $auth->id;
         	$ciclo->update();
         	return redirect("listar/ciclo");
     	}
