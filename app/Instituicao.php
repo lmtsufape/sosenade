2e112fcfae15c89b34408a977eda93ca530e8391
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-03-13 11:48:29 -0300

    add rules e messages ao model instituicao

--- app/Instituicao.php
+++ app/Instituicao.php
@@ -14,10 +14,20 @@ class Instituicao extends Authenticatable
 
     public static $rules = 
     [
-
+        'nome'  => 'required',
+    	'cnpj' => 'required|min:18',
+    	'password' => 'required|min:8|confirmed',
+    	'email' => 'required|email',
+    	// 'tipousuario_id' => 'required',
     ];
 
     public static $messages = [
-
+    	'required' => 'O campo :attribute deve ser preenchido na forma correta',
+    	'cnpj.min' => 'O :attribute deve conter no minimo 18 caracteres',
+    	'password.min' => 'A senha deve ter no minimo 8 caracteres',
+    	'email.email' => "O email deve ser um email valido",
+        'unique' => "O :attribute já esta cadastrado no sistema!!",
+        'password.confirmed' => "As senhas devem ser identicas"
     ];
+    
 }
