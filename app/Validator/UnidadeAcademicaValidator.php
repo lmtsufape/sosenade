b1f5fc7645ddfb6dee8e2a52633a0da678daa7a6
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-03-24 03:23:03 -0300

    add linha de validator anterior  no arquivo

--- app/Validator/UnidadeAcademicaValidator.php
+++ app/Validator/UnidadeAcademicaValidator.php
@@ -2,15 +2,16 @@
 
 namespace SimuladoENADE\Validator;
 use SimuladoENADE\UnidadeAcademica;
-class InstituicaoValidator
-{
+
+class UnidadeAcademicaValidator {
 
 	public static function validate($dados)
     {
-    	$validator = \Validator::make($dados, UnidadeAcademica::$rules, UnidadeAcademica::$messages);
+		// $validator = \Validator::make($dados, ['nome' => "required|unique:unidade_academicas,nome,".$dados['id'],], UnidadeAcademica::$messages);
+		$validator = \Validator::make($dados, UnidadeAcademica::$rules, UnidadeAcademica::$messages);
 
     	if(!$validator->errors()->isEmpty()){
-    		throw new ValidationException($validator, "Erro ao validar Instituição");
+    		throw new ValidationException($validator, "Erro ao validar Unidade Academica");
     	}
     }
 }
