3d5c734349b8555db6258af2bba8133c94e1aa74
Edgarvital <edgar.tecnico@hotmail.com>
2021-03-25 16:46:05 -0300

    Redesign das paginas de cadastrar e listar disciplinas

--- resources/views/DisciplinaView/cadastrarDisciplinas.blade.php
+++ resources/views/DisciplinaView/cadastrarDisciplinas.blade.php
@@ -2,31 +2,38 @@
 @section('titulo','Cadastrar Disciplinas')
 @section('content')
 
-	<form class="shadow p-3 bg-white rounded" action="{{route('add_disciplina')}}" method="post">
+    <div class="shadow p-3 bg-white" style="border-radius: 10px; width: 80%; margin-left: auto; margin-right: auto">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col" align="left">
+                <h1 style="margin-left: 15px; margin-top: 15px"> Cadastro de Disciplinas </h1>
+                <p style="color: #606f7b; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Inicio</a> >
+                    Cadastrar Disciplina
+                </p>
+            </div>
+        </div>
+
+	<form action="{{route('add_disciplina')}}" method="post">
 		<input type="hidden" name="_token" value="{{csrf_token()}}">
-		
-		<h1 class="text-center"> Cadastrar Disciplina/Conteúdo </h1>
-		<h2 class="text-center">
-			@if (Auth::guard('aluno')->user())
-				{{Auth::guard('aluno')->user()->curso->curso_nome}}
-			@elseif (Auth::user())
-				{{Auth::user()->curso->curso_nome}}
-			@endif
-		</h2><br>
-		
+
 		<div class="form-group justify-content-center row">
-			<div class="col-md-6">
+			<div class="col-md-10">
+                <label for="nome">Nome da Disciplina/Conteudo</label>
 				<input type="text" name="nome" id="nome" placeholder="Digite o nome da disciplina/conteúdo aqui" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" value="{{ old('nome') }}" required autofocus>
 					@if ($errors->has('nome'))
 						<span class = "invalid-feedback" role="alert">
 							<strong>{{$errors->first('nome')}}</strong>
 						</span>
 					@endif
-			</div> 
+			</div>
 		</div>
 
-		<div class="col-md-12 text-center">
-			<br><button type="submit" name="cadastrar" class="btn btn-primary">Cadastrar</button><br><br>
-		</div>
+        <hr style="width: 67%; margin-top: 30%">
+        <div class="row" style="margin-left: 60%; margin-top: -15px">
+            <div class="text-center my-3" id="btn_cadastrar">
+                <button type="submit" name="cadastrar" class="btn btn-primary" style="width: 200px">Cadastrar Disciplina</button>
+            </div>
+        </div>
 	</form>
 @stop
