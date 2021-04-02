0e5ae4c5d6c39faf2e20dc454d384691fd29bb2c
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-04-02 13:33:44 -0300

    add correções de conflito de unidades academicas

--- app/UnidadeAcademica.php
+++ app/UnidadeAcademica.php
@@ -5,7 +5,42 @@ namespace SimuladoENADE;
 use Illuminate\Database\Eloquent\Model;
 
 class UnidadeAcademica extends Model
-{
+{   
+    protected $table = 'unidade_academicas';
+
+    protected $fillable = ['nome', 'instituicao_id'];
+
+    public static $rules = [
+        'nome' => 'required',
+    ];
+
+    public static $messages = [
+        'required' => 'O campo :attribute deve ser preenchido na forma correta',
+    ];
+
+    public static function queryToStringIds($values) {
+        
+        $str = '(';
+        
+        foreach($values as $value)
+            $str = $str.$value->id.',';
+        
+        $virgula_final = (strlen($str)-1);
+        $str[$virgula_final] = ')';
+
+        return $str;
+    }
+
+    public static function queryToArrayIds($values) {
+        
+        $array = [];
+
+        foreach($values as $value)
+            array_push($array, $value->id);
+        
+        return $array;
+    }
+
     public function cursos(){
         return $this->hasMany('SimuladoENADE\Curso', 'unidade_id');
     }
