20f3c5db902ea88cb69c2a0056cff7eed1484b4d
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-04-04 04:00:21 -0300

    add migrations para correção de quebra de integridade de dados em tabelas que implementavam a constrain onDelete(set null) que causava inconsistencia de dados

---
+++ database/migrations/2021_04_04_054125_chave_curso_unidade_and_ciclo_less_ondelete.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChaveCursoUnidadeAndCicloLessOndelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cursos', function (Blueprint $table) {
+            $table->dropForeign('cursos_unidade_id_foreign');
+            $table->dropForeign('cursos_ciclo_id_foreign');
+            $table->foreign('ciclo_id')->references('id')->on('ciclos');
+            $table->foreign('unidade_id')->references('id')->on('unidade_academicas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cursos', function (Blueprint $table) {
+            $table->dropForeign('cursos_unidade_id_foreign');
+            $table->dropForeign('cursos_ciclo_id_foreign');
+            $table->foreign('ciclo_id')->references('id')->on('ciclos');
+            $table->foreign('unidade_id')->references('id')->on('unidade_academicas');
+        });
+    }
+}
