8e425de170837f636f32f4feeb5c39225f780a5c
Edgarvital <edgar.tecnico@hotmail.com>
2021-04-20 13:55:57 -0300

    Redesign Paginas do Curso

--- resources/views/CursoView/cadastrarCursos.blade.php
+++ resources/views/CursoView/cadastrarCursos.blade.php
@@ -1,56 +1,78 @@
 @extends('layouts.app')
 @section('titulo','Cadastrar Curso')
 @section('content')
-	
-	<form class="shadow p-3 bg-white rounded" action= "{{route('add_curso')}}" method="post">
-		<h1 class="text-center"> Cadastrar Curso </h1><br>
-		<input type="hidden" name="_token" value="{{csrf_token()}}">
-		<div class="form-row col-md-12 justify-content-center">
-			<div class="form-group col-md-4">
-				<label for="curso_nome">Nome do Curso</label>
-				<input type="text" class="form-control{{ $errors->has('curso_nome') ? ' is-invalid' : '' }}"  name="curso_nome" id="curso_nome" placeholder="Digite o nome do curso aqui" value="{{ old('curso_nome') }}" required autofocus>
-				@if ($errors->has('curso_nome'))
-					<span class = "invalid-feedback" role="alert">
+
+    <div class="shadow p-3 bg-white" style="border-radius: 10px; width: 80%; margin-left: auto; margin-right: auto">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col" align="left">
+                <h1 style="margin-left: 15px; margin-top: 15px"> Cadastro de Curso </h1>
+                <p style="color: #606f7b; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Inicio</a> >
+                    Cadastrar Curso
+                </p>
+            </div>
+        </div>
+
+        <form action="{{route('add_curso')}}" method="post">
+            <h1 class="text-center"> Cadastrar Curso </h1><br>
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <div class="form-row col-md-12 justify-content-center">
+                <div class="form-group col-md-4">
+                    <label for="curso_nome">Nome do Curso</label>
+                    <input type="text" class="form-control{{ $errors->has('curso_nome') ? ' is-invalid' : '' }}"
+                           name="curso_nome" id="curso_nome" placeholder="Digite o nome do curso aqui"
+                           value="{{ old('curso_nome') }}" required autofocus>
+                    @if ($errors->has('curso_nome'))
+                        <span class="invalid-feedback" role="alert">
 						<strong>{{$errors->first('curso_nome')}}
 					</span>
-				@endif
-			</div> 
-		
-			<div class="form-group col-md-4">
-				<label for="ciclo_id">Ciclo</label>
-				<select class="form-control{{ $errors->has('ciclo_id') ? ' is-invalid' : '' }}" name="ciclo_id" required autofocus>
-					<option hidden disabled selected>Selecione o ciclo</option>
-					@foreach ($ciclos as $ciclo)
-						<option value="{{$ciclo->id}}" {{ old('ciclo') == $ciclo->id ? 'selected' : ''}}   >{{$ciclo->tipo_ciclo}}</option>
-					@endforeach
-				</select>
-				@if ($errors->has('ciclo_id'))
-					<span class = "invalid-feedback" role="alert">
+                    @endif
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="ciclo_id">Ciclo</label>
+                    <select class="form-control{{ $errors->has('ciclo_id') ? ' is-invalid' : '' }}" name="ciclo_id"
+                            required autofocus>
+                        <option hidden disabled selected>Selecione o ciclo</option>
+                        @foreach ($ciclos as $ciclo)
+                            <option
+                                value="{{$ciclo->id}}" {{ old('ciclo') == $ciclo->id ? 'selected' : ''}} >{{$ciclo->tipo_ciclo}}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('ciclo_id'))
+                        <span class="invalid-feedback" role="alert">
 						{{$errors->first('ciclo_id')}}
 					</span>
-				@endif
-			</div>
-		</div>
+                    @endif
+                </div>
+            </div>
 
-		<div class="form-row col-md-12 justify-content-center">
-			<div class="form-group col-md-4">
-				<label for="unidade_id">Unidade</label>
-				<select class="form-control{{ $errors->has('unidade_id') ? ' is-invalid' : '' }}" name="unidade_id" required autofocus>
-					<option hidden disabled selected>Selecione a unidade</option>
-					@foreach($unidade_academicas as $unidade_academica)
-						<option value="{{$unidade_academica->id}}" {{ old('unidade_academica') == $unidade_academica->id ? 'selected' : ''}}>{{$unidade_academica->nome}}</option>
-					@endforeach
-				</select>
-				@if ($errors->has('unidade_id'))
-					<span class = "invalid-feedback" role="alert">
+            <div class="form-row col-md-12 justify-content-center">
+                <div class="form-group col-md-4">
+                    <label for="unidade_id">Unidade</label>
+                    <select class="form-control{{ $errors->has('unidade_id') ? ' is-invalid' : '' }}" name="unidade_id"
+                            required autofocus>
+                        <option hidden disabled selected>Selecione a unidade</option>
+                        @foreach($unidade_academicas as $unidade_academica)
+                            <option
+                                value="{{$unidade_academica->id}}" {{ old('unidade_academica') == $unidade_academica->id ? 'selected' : ''}}>{{$unidade_academica->nome}}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('unidade_id'))
+                        <span class="invalid-feedback" role="alert">
 						{{$errors->first('unidade_id')}}
 					</span>
-				@endif
-			</div>
-		</div>
-		
-		<div class="col-md-12 text-center">
-			<br><button type="submit" name="cadastrar" class="btn btn-primary">Cadastrar</button><br><br>
-		</div>
-	</form>
+                    @endif
+                </div>
+            </div>
+
+            <hr style="width: 67%;">
+            <div class="row" style="margin-left: 60%; margin-top: -15px">
+                <div class="text-center my-3" id="btn_cadastrar">
+                    <button type="submit" name="cadastrar" class="btn btn-primary" style="width: 200px">Cadastrar Curso</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @stop
--- resources/views/CursoView/listaCursos.blade.php
+++ resources/views/CursoView/listaCursos.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
-@section('titulo','Ciclos Cadastrados')
+@section('titulo','Cursos Cadastrados')
 @section('content')
-    <div class="shadow p-4 bg-white rounded container-fluid" style="overflow-x: auto;">
+    <div class="shadow p-3 bg-white" style="border-radius: 10px">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col-sm">
+                <h1 style="margin-left: 15px; margin-top: 15px">Cursos Cadastrados</h1>
+                <p style="color: #606f7b; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Inicio</a> >
+                    Cursos Cadastrados
+                </p>
+            </div>
+            <div class="col-sm" style="margin-top: 30px; margin-right: 20px">
+                <a class="btn btn-primary" href="{{route('new_curso')}}" style="float: right;"> Inserir
+                    Curso</a><br>
+            </div>
 
-		<h1 class="text-center"> Cursos </h1><br>
+        </div>
 
 		@if (session('success'))
 			<div class="alert alert-success">
@@ -16,13 +29,13 @@
 		@endif
 
 		@if(!$cursos->isEmpty())
-			<table id="tabela_dados" class="table table-hover">
+			<table id="tabela_dados" class="table table-hover" style="border-style: groove; border-color: #6cb2eb">
 		 		<thead>
-					<tr>
+					<tr class="header" style="background: #1B2E4F; color: white">
 						<th>Nome do Curso</th>
 						<th>Ciclo</th>
 						<th>Unidade</th>
-						<th style="width: 7%">Opções</th>
+						<th style="width: 10%">Opções</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -32,9 +45,9 @@
                                 <td>{{$curso->curso_nome}}</td>
                                 <td>{{$curso->tipo_ciclo}}</td>
                                 <td>{{$curso->unidade->nome}}</td>
-                                <td class="btn-group">
-                                    <a href="{{route('edit_curso',['id'=>$curso->curso_id])}}" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i></a>
-                                    <a onclick="return confirm('Você tem certeza que deseja excluir?')" href="{{route('delete_curso',['id'=>$curso->curso_id])}}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+                                <td>
+                                    <a href="{{route('edit_curso',['id'=>$curso->curso_id])}}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+                                    <a onclick="return confirm('Você tem certeza que deseja excluir?')" href="{{route('delete_curso',['id'=>$curso->curso_id])}}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
                                 </td>
                             </tr>
                         @endif
@@ -45,11 +58,17 @@
 			<p class="text-center alert alert-light">Não existem cursos cadastrados até o momento.</p>
 		@endif
 
-		<hr class="star-light">
+        <hr>
 
-		<div class="text-center mt-5">
-			<a class="btn btn-primary" href="{{route('new_curso')}}"> Adicionar um novo curso </a><br><br>
-		</div>
+        <p>Legenda:</p>
+        <a class="btn btn-primary"
+           data-placement="bottom" rel="tooltip" title="Editar" style="color: white; margin-left: 5px"><i
+                class="fa fa-pencil"></i></a>
+        Editar Curso
+        <a class="btn btn-danger"
+           data-placement="bottom" rel="tooltip" title="Excluir" style="color: white; margin-left: 5px"><i
+                class="fa fa-trash"></i></a>
+        Deletar Curso
 	</div>
 
 	<script type="text/javascript">
