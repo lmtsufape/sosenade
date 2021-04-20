aa7b8d05537a9417535aecb7c3050c889551c480
Edgarvital <edgar.tecnico@hotmail.com>
2021-04-20 13:55:08 -0300

    Redesign Paginas da Unidade

--- resources/views/UnidadeView/cadastrarUnidade.blade.php
+++ resources/views/UnidadeView/cadastrarUnidade.blade.php
@@ -1,27 +1,47 @@
 @extends('layouts.app')
 @section('titulo','Cadastrar Unidade')
 @section('content')
-	<form class="shadow p-3 bg-white rounded" action="{{ route('add_unidade') }}" method="post">
-	
-		<input type="hidden" name="id" value="-1">
-		<input type="hidden" name="_token" value="{{csrf_token()}}">
 
-		<h1 class="text-center"> Cadastrar Unidade </h1>
-		<br>
+    <div class="shadow p-3 bg-white" style="border-radius: 10px; width: 80%; margin-left: auto; margin-right: auto">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col" align="left">
+                <h1 style="margin-left: 15px; margin-top: 15px"> Cadastro de Unidade </h1>
+                <p style="color: #606f7b; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Inicio</a> >
+                    Cadastrar Unidade
+                </p>
+            </div>
+        </div>
+        <form action="{{ route('add_unidade') }}" method="post">
 
-		<div class="form-group justify-content-center row">
-			<div class="form-group col-md-8">
-				<label for="nome">Nome</label>
-				<input type="text" name="nome" id="nome" placeholder="Nome da Unidade" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" value="{{ old('nome') }}" required autofocus>
-				@if ($errors->has('nome'))
-					<span class = "invalid-feedback" role="alert">
+            <input type="hidden" name="id" value="-1">
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+
+            <h1 class="text-center"> Cadastrar Unidade </h1>
+            <br>
+
+            <div class="form-group justify-content-center row">
+                <div class="form-group col-md-8">
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nome" id="nome" placeholder="Nome da Unidade"
+                           class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" value="{{ old('nome') }}"
+                           required autofocus>
+                    @if ($errors->has('nome'))
+                        <span class="invalid-feedback" role="alert">
 						{{$errors->first('nome')}}
 					</span>
-				@endif
-			</div>
+                    @endif
+                </div>
+            </div>
 
-		<div class="col-md-12 text-center">
-			<button type="submit" name="cadastrar" class="btn btn-primary">Cadastrar</button><br><br>
-		</div>
-	</form>
+                <hr style="width: 67%;">
+                <div class="row" style="margin-left: 60%; margin-top: -15px">
+                    <div class="text-center my-3" id="btn_cadastrar">
+                        <button type="submit" name="cadastrar" class="btn btn-primary" style="width: 200px">Cadastrar Unidade</button>
+                    </div>
+                </div>
+
+        </form>
+    </div>
 @stop
--- resources/views/UnidadeView/editarUnidade.blade.php
+++ resources/views/UnidadeView/editarUnidade.blade.php
@@ -1,27 +1,42 @@
 @extends('layouts.app')
 @section('titulo','Editar Unidade')
 @section('content')
-	<form class="shadow p-3 bg-white rounded" action="{{ route('update_unidade') }}" method="post">
-	
-		<input type="hidden" name="id" value="{{ $unidade->id }}">
-		<input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div class="shadow p-3 bg-white" style="border-radius: 10px; width: 80%; margin-left: auto; margin-right: auto">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col" align="left">
+                <h1 style="margin-left: 15px; margin-top: 15px"> Editar Unidade </h1>
+                <p style="color: #606f7b; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Inicio</a> >
+                    Editar Unidade
+                </p>
+            </div>
+        </div>
+        <form action="{{ route('update_unidade') }}" method="post">
 
-		<h1 class="text-center"> Editar Unidade </h1>
-		<br>
+            <input type="hidden" name="id" value="{{ $unidade->id }}">
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
 
-		<div class="form-group justify-content-center row">
-			<div class="form-group col-md-8">
-				<label for="nome">Nome</label>
-				<input type="text" name="nome" id="nome" placeholder="Nome da Unidade" class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}" value="{{ $unidade->nome }}" required autofocus>
-				@if ($errors->has('nome'))
-					<span class = "invalid-feedback" role="alert">
+            <div class="form-group justify-content-center row">
+                <div class="form-group col-md-8">
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nome" id="nome" placeholder="Nome da Unidade"
+                           class="form-control{{ $errors->has('nome') ? ' is-invalid' : '' }}"
+                           value="{{ $unidade->nome }}" required autofocus>
+                    @if ($errors->has('nome'))
+                        <span class="invalid-feedback" role="alert">
 						{{$errors->first('nome')}}
 					</span>
-				@endif
-			</div>
+                    @endif
+                </div>
+            </div>
 
-		<div class="col-md-12 text-center">
-			<button type="submit" name="cadastrar" class="btn btn-primary"> Salvar alterações </button><br><br>
-		</div>
-	</form>
+            <hr style="width: 67%">
+            <div class="row" style="margin-left: 60%; margin-top: -15px">
+                <div class="text-center my-3" id="btn_cadastrar">
+                    <button type="submit" name="cadastrar" class="btn btn-primary" style="width: 200px">Salvar Alterações</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @stop
