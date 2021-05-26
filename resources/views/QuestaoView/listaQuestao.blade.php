c5c50070d1b2c8f8a7179d65111d2506b5aacbbc
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-05-26 11:10:34 -0300

    alterando modal de questões para tamanho maior

--- resources/views/QuestaoView/listaQuestao.blade.php
+++ resources/views/QuestaoView/listaQuestao.blade.php
@@ -80,7 +80,7 @@
                             <!-- Modal -->
                             <div class="modal fade" id="modal_{{$questao->qstid}}" tabindex="-1" role="dialog"
                                  aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title"
