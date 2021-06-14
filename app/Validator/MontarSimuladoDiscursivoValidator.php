55c978adb153c26ec70a43d74998d1672abb897a
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-06-14 20:40:28 -0300

    add MontarSimuladoDiscursivoValidator

---
+++ app/Validator/MontarSimuladoDiscursivoValidator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SimuladoENADE\Validator;
+
+use SimuladoENADE\SimuladoAluno;
+use SimuladoENADE\Validator\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
+class MontarSimuladoDiscursivoValidator
+{
+
+	public static function validate($qtd_existente, $qtd_exigido) {
+
+    	$validator = Validator::make($rules = [], $messages = []);
+
+    	if($qtd_existente < $qtd_exigido){
+    		$validator->errors()->add('qtd_maior', 'A quantidade de questões exigidas ('.$qtd_exigido.') está muito grande. A quantidade máxima de questões compatíveis com os filtros é '.$qtd_existente.'.'); 
+    	}
+
+    	if(!$validator->errors()->isEmpty()){
+    		throw new ValidationException($validator, "Erro ao validar um novo Simulado Aluno");
+    	}
+
+    }
+}
