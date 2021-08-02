d5ba5d1ed42ab773ec8d81532f97c0c37ba30c36
Edgarvital <edgar.tecnico@hotmail.com>
2021-08-02 14:21:27 -0300

    Alteração na ordem do Style

--- resources/views/QuestaoView/importarQuestaoDiscursiva.blade.php
+++ resources/views/QuestaoView/importarQuestaoDiscursiva.blade.php
@@ -2,6 +2,124 @@
 @section('titulo','Importar Questão')
 @section('content')
 
+    <style>
+
+        .toggle-check-input {
+            width: 1px;
+            height: 1px;
+            position: absolute;
+        }
+
+        .toggle-check-text {
+            display: inline-block;
+            position: relative;
+            text-transform: uppercase;
+            background: #CCC;
+            padding: 0.25em 0.5em 0.25em 2em;
+            border-radius: 1em;
+            min-width: 2em;
+            color: #FFF;
+            cursor: pointer;
+            transition: background-color 0.15s;
+        }
+
+        .toggle-check-text:after {
+            content: ' ';
+            display: block;
+            background: #FFF;
+            width: 1.1em;
+            height: 1.1em;
+            border-radius: 1em;
+            position: absolute;
+            left: 0.3em;
+            top: 0.25em;
+            transition: left 0.15s, margin-left 0.15s;
+        }
+
+        .toggle-check-text:before {
+            content: 'Nenhum';
+        }
+
+        .toggle-check-input:checked ~ .toggle-check-text {
+            background: #2196F3;
+            padding-left: 0.5em;
+            padding-right: 2em;
+        }
+
+        .toggle-check-input:checked ~ .toggle-check-text:before {
+            content: 'Todos';
+        }
+
+        .toggle-check-input:checked ~ .toggle-check-text:after {
+            left: 100%;
+            margin-left: -1.4em;
+        }
+    </style>
+    <style>
+
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+
+        /* Hide default HTML checkbox */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        /* The slider */
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        input:checked + .slider {
+            background-color: #2196F3;
+        }
+
+        input:focus + .slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked + .slider:before {
+            -webkit-transform: translateX(26px);
+            -ms-transform: translateX(26px);
+            transform: translateX(26px);
+        }
+
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 34px;
+        }
+
+        .slider.round:before {
+            border-radius: 50%;
+        }
+    </style>
+
     @if(Session::has('success'))
         <div class="alert alert-success">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
@@ -199,124 +317,6 @@
             </div>
         </div>
     </div>
-
-    <style>
-
-        .toggle-check-input {
-            width: 1px;
-            height: 1px;
-            position: absolute;
-        }
-
-        .toggle-check-text {
-            display: inline-block;
-            position: relative;
-            text-transform: uppercase;
-            background: #CCC;
-            padding: 0.25em 0.5em 0.25em 2em;
-            border-radius: 1em;
-            min-width: 2em;
-            color: #FFF;
-            cursor: pointer;
-            transition: background-color 0.15s;
-        }
-
-        .toggle-check-text:after {
-            content: ' ';
-            display: block;
-            background: #FFF;
-            width: 1.1em;
-            height: 1.1em;
-            border-radius: 1em;
-            position: absolute;
-            left: 0.3em;
-            top: 0.25em;
-            transition: left 0.15s, margin-left 0.15s;
-        }
-
-        .toggle-check-text:before {
-            content: 'Nenhum';
-        }
-
-        .toggle-check-input:checked ~ .toggle-check-text {
-            background: #2196F3;
-            padding-left: 0.5em;
-            padding-right: 2em;
-        }
-
-        .toggle-check-input:checked ~ .toggle-check-text:before {
-            content: 'Todos';
-        }
-
-        .toggle-check-input:checked ~ .toggle-check-text:after {
-            left: 100%;
-            margin-left: -1.4em;
-        }
-    </style>
-    <style>
-
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-        }
-
-        /* Hide default HTML checkbox */
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
-
-        /* The slider */
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-
-        input:checked + .slider {
-            background-color: #2196F3;
-        }
-
-        input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
-        }
-
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-        }
-
-        /* Rounded sliders */
-        .slider.round {
-            border-radius: 34px;
-        }
-
-        .slider.round:before {
-            border-radius: 50%;
-        }
-    </style>
 
     <script>
         //Funcionalidade de Selecionar Todos
