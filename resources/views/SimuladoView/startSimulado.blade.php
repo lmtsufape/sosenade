ff81343660da42844d4cffa167eb97efa9df5cef
Edgarvital <edgar.tecnico@hotmail.com>
2021-08-11 21:14:33 -0300

    Redesign da Tela de Start Simulado

--- resources/views/SimuladoView/startSimulado.blade.php
+++ resources/views/SimuladoView/startSimulado.blade.php
@@ -2,16 +2,25 @@
 @section('titulo','Simulado')
 @section('content')
 
-	<div class="shadow p-3 bg-white rounded">
+    <div class="shadow p-3 bg-white" style="border-radius: 10px">
+        <div class="row"
+             style="background: #1B2E4F; margin-top: -15px; margin-bottom:  30px; border-radius: 10px 10px 0 0; color: white">
+            <div class="col-sm">
+                <h1 style="margin-left: 15px; margin-top: 15px">Responder Simulado - {{$simulado->descricao_simulado}}</h1>
+                <p style="color: #9fcdff; margin-left: 15px; margin-top: -5px">
+                    <a href="{{route('home')}}" style="color: inherit;">Início</a> >
+                    Simulados Disponiveis > Responder Simulado
+                </p>
+            </div>
+        </div>
 
 		<div class="col-md-12 text-center">
-			<h2>Responder simulado - {{$simulado->descricao_simulado}}</h2><br>
 			<h5>Você tem <b>{{count($questaos)}}</b> questões a responder neste simulado </h5><br>
 		</div>
 
 		<table class="table form-group justify-content-center table-striped" id="dynamic_field" >
 			<thead>
-				<tr>
+            <tr style="color: gray;" >
 					<th style="text-align: center;">#</th>
 					<th>Disciplina</th>
 					<th style="text-align: center;">Nível</th>
