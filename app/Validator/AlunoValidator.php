07fef301250b301bea44adaf9d974d959a5f745a
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2021-08-24 16:18:41 -0300

    add validators de senha em AlunoValidator

--- app/Validator/AlunoValidator.php
+++ app/Validator/AlunoValidator.php
@@ -9,11 +9,14 @@ class AlunoValidator
     {
 
     	$validator = \Validator::make($dados,
-    								 [    	
+    							[    	
     								'nome'  => 'required|',
 							    	'cpf' => 'required|min:14|unique:alunos,cpf,'.$dados['id'],
-							    	'email' => 'required|email|unique:alunos,email,'.$dados['id'],],
-    								 Aluno::$messages);
+							    	'email' => 'required|email|unique:alunos,email,'.$dados['id'],
+									'password' => 'required|min:8|required_with:password_confirmation',
+									'password_confirmation' => 'required_with:password|same:password'
+								],
+    							 Aluno::$messages);
     	if(!$validator->errors()->isEmpty()){
     		throw new ValidationException($validator, "Erro ao validar um Aluno");
     	}
