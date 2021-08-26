56f9959dba4d0aa21136d04ae39730f6a9d9cf9c
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2021-08-26 20:52:03 -0300

    add redesign de view

--- resources/views/AlunoView/mudarSenha.blade.php
+++ resources/views/AlunoView/mudarSenha.blade.php
@@ -7,11 +7,6 @@
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
             </button>{{ Session::get('message', '') }}
         </div>
-    @elseif(Session::has('fail'))
-        <div class="alert alert-danger">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-            </button> {{ Session::get('message', '') }}
-        </div>
     @endif
 
     <div class="shadow p-3 bg-white text-center" style="border-radius: 10px">
@@ -42,8 +37,8 @@
                                        placeholder="Digite a sua senha atual" class="form-control"
                                        value="{{ old('old_password') }}" required autofocus>
                                 @if(Session::has('fail'))
-                                    <span class="invalid-feedback" role="alert">
-												{{ Session::get('message', '') }}
+                                    <span style="color: red; font-size: 14px;">
+												{{ Session::get('message', '')}}
 											</span>
                                 @endif
                             </div>
@@ -63,49 +58,28 @@
                                 @endif
                             </div>
 
-					<input type="hidden" name="id" value="{{$aluno->id}}">
-					<input type="hidden" name="_token" value="{{csrf_token()}}">
-
-					<div class="form-group justify-content-center row">
-						<div class="form-row col-md-12 justify-content-center">
-							<div class="form-group col-md-4">
-								<label for="old_password">Senha atual</label>
-								<input type="password" id="old_password" name="old_password" placeholder="Digite a sua senha atual" class="form-control" value="{{ old('old_password') }}" required autofocus>
-								@if ($errors->has('old_password'))
-									<span class = "invalid-feedback" role="alert">
-										{{$errors->first('old_password')}}
-									</span>
-								@endif
-							</div>
-							<div class="form-group col-md-4">
-								<label for="password">Nova senha</label>
-								<input type="password" id="password" name="password" placeholder="Digite a nova senha" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" value="{{ old('password') }}" required autofocus>
-								@if ($errors->has('password'))
-									<span class = "invalid-feedback" role="alert">
-										{{$errors->first('password')}}
-									</span>
-								@endif
-							</div>
-							<div class="form-group col-md-4">
-								<label for="password_confirmation">Confirme a nova senha</label>
-								<input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirme sua nova senha" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" value="{{ old('password_confirmation') }}" required autofocus>
-								@if ($errors->has('password_confirmation'))
-									<span class = "invalid-feedback" role="alert">
-										{{$errors->first('password_confirmation')}}
-									</span>
-								@endif
-							</div>
-						</div>
-					</div>
-					<div class="justify-content-center row">
-						<div class="text-center my-3" id="btn_alterar">
-							<button type="submit" name="alterar" class="btn btn-primary">Salvar senha</button>
-						</div>
-					</div>
-				</form>
-
-			</div>
-		</div>
-
-	</div>
+                            <div class="form-group col-md-12">
+                                <label for="password_confirmation" style="float: left;">Confirme a nova
+                                    senha</label>
+                                <input type="password" id="password_confirmation" name="password_confirmation"
+                                       placeholder="Confirme sua nova senha"
+                                       class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
+                                       value="{{ old('password_confirmation') }}" required autofocus>
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="invalid-feedback" role="alert">
+												{{$errors->first('password_confirmation')}}
+											</span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="justify-content-center row">
+                        <div class="text-center my-3" id="btn_alterar">
+                            <button type="submit" name="alterar" class="btn btn-primary">Salvar senha</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
